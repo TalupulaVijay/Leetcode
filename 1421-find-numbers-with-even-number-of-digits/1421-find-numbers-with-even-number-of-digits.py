fc4457ab77a8ddd4e0078af3916b8177c656baca
TalupulaVijay <vijaychinnuvijay32@gmail.com>
2025-08-22 19:48:22 +0530

    Time: 0 ms (100%), Space: 12.3 MB (93.19%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findNumbers(self, nums):
+        c=0
+        for i in nums:
+            if len(str(i))%2==0:
+                c+=1
+        return c        
+
+        
