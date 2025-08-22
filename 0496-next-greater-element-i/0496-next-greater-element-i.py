240ea0c4f9d8808af36d684075817e1578a5670b
TalupulaVijay <vijaychinnuvijay32@gmail.com>
2025-08-22 19:12:24 +0530

    Time: 31 ms (30.64%), Space: 12.5 MB (85%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        res=[]
+        for i in nums1:
+            index=nums2.index(i)
+            found=-1
+            for j in range(index+1,len(nums2)):
+                if nums2[j]>i:
+                    found=nums2[j]
+                    break
+            res.append(found)  
+        return res         
+
+        
