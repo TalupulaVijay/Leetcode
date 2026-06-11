b97465b5c39ef8a736203da8b9ed867c28020e91
TalupulaVijay <vijaychinnuvijay32@gmail.com>
2026-06-11 10:03:47 +0530

    Time: 79 ms (9.32%), Space: 28.6 MB (42.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def maxProfit(self, prices):
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         l,r=0,1
-        m=0
+        max_proof=0
         while r!=len(prices):
-            if prices[l]<=prices[r]:
-                d=prices[r]-prices[l]
-                m=max(m,d)
+            if prices[l]<prices[r]:
+                proof=prices[r]-prices[l]
+                max_proof=max(max_proof,proof)
             else:
                 l=r
-            r+=1        
-        return m        
-
+            r+=1
+        return max_proof
         
